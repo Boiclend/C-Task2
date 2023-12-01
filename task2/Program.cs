c8efafaf423a88e639055ea099e68317a2edecba
Roman <user@email.com>
2023-12-01 19:56:17 +0500

    added task2

---
+++ task2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Используя показатели функции вычислить цепную дробь. Количество елементов дроби надо задавать с клавиатуры.
+// Например, возьмем количество элементов дроби, равное — 6. Вот такое выражение должно у вас получится: 1+1/(1+1/(1+1/(1+1/(1+1/(1+1))))).
+// А вот и правильный ответ: 1,61538.
+
+double Message(string message) {
+    Console.WriteLine(message);
+    double num = Convert.ToDouble(Console.ReadLine());
+    return num;
+}
+
+double Drob(double num) {
+    int count = 1;
+    double result = 2;
+        while(count < num) {
+            result = 1 + 1 / result;
+            count++;
+        }
+    return result;
+}
+
+double number = Message("Введите количество цепной дроби");
+double Result = Drob(number);
+
+Console.WriteLine(Result);
+
